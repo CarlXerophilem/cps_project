0b397b73a04b93819d1732c8d64b41c18429d96a
CarlXerophilem <145671412+CarlXerophilem@users.noreply.github.com>
2025-11-22 00:34:26 +0800

    Delete FlightBookingSystem directory

--- FlightBookingSystem/src/payment/CreditCardPayment.java
+++
@@ -1,8 +0,0 @@
-package payment;
-
-public class CreditCardPayment implements Payment {
-    @Override
-    public void processPayment(double amount) {
-        System.out.println("Payment of $" + amount + " made using Credit Card.");
-    }
-}
