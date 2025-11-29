ea4b1fc41a2bc0465d8a9888048ce8153aea8db7
CarlXerophilem <145671412+CarlXerophilem@users.noreply.github.com>
2025-11-29 13:15:13 +0800

    Delete FlightBookingSystem directory

--- FlightBookingSystem/src/payment/PayPalPayment.java
+++
@@ -1,8 +0,0 @@
-package payment;
-
-public class PayPalPayment extends Payment {
-    @Override
-    public void processPayment(String flightNum) {
-        System.out.println("Payment of $" + importPrice(flightNum) + "should be made with PayPal.");
-    }
-}
